f5cc04193756e74df58f1d916334d3a7b118dad1
UserOne <userone@users-MacBook-Pro.local>
2016-11-13 15:08:12 +0200

    second commits fragmrnt toast

--- app/src/main/java/com/example/adex/navigationdrawer1/MainActivity.java
+++ app/src/main/java/com/example/adex/navigationdrawer1/MainActivity.java
@@ -1,6 +1,6 @@
 package com.example.adex.navigationdrawer1;
 
-import android.support.v7.app.AppCompatActivity;
+//import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 //public class MainActivity extends AppCompatActivity {
--- app/src/main/java/fragments/ScreenThree.java
+++ app/src/main/java/fragments/ScreenThree.java
@@ -13,6 +13,15 @@ import android.view.ViewGroup;
 import com.example.adex.navigationdrawer1.R;
 //import com.example.adex.navigationdrawer1.R;
 
+import android.os.Bundle;
+//import android.app.Fragment;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
 
 public class ScreenThree extends Fragment {
 
@@ -23,9 +32,35 @@ public class ScreenThree extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        View rootView = inflater.inflate(R.layout.screen_third, container,
-                false);
 
-        return rootView;
+
+        // Inflate the layout for this fragment
+        //  View rootView = inflater.inflate(R.layout.screen_third, container, false);
+        View view = inflater.inflate(R.layout.screen_third,null,false);
+
+        // get the widgets reference from Fragment XML layout
+        Button btn_fragment = (Button) view.findViewById(R.id.btn_fragment);
+
+        // Set a click listener for Fragment button
+        btn_fragment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //.............................................................
+                // Create a Toast notification/message
+                Toast toast = Toast.makeText(
+                        //getActivity(),"Custom Toast From Fragment",Toast.LENGTH_LONG
+                        getActivity().getApplicationContext(), "Custom Toast From Fragment", Toast.LENGTH_LONG
+                );
+                // Set the Toast display position layout center
+                toast.setGravity(Gravity.CENTER,0,0);
+                // Finally, show the toast
+                toast.show();
+                //.............................................................
+            }
+        });
+
+        //// Return the view as Fragment layout
+       // return rootView;
+        return view;
     }
 }
